d5e4faf6215ebb6383562f301d509dd40b7ba32b
shubham thakur <shubhamthakur60278@gmail.com>
2026-05-17 23:33:19 +0530

    feat(docker): update Dockerfile to set DATABASE_URL for Prisma client generation

--- backend/prisma.config.js
+++ backend/prisma.config.js
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import { defineConfig } from '@prisma/config';
-import { DATABASE_URL } from './src/constants/index.js';
+
 export default defineConfig({
     datasource: {
-        url: DATABASE_URL,
+        url: process.env.DATABASE_URL,
     },
 });
